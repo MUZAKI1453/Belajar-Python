0845794cf638d798523cc6f7a58694dc3e40c11c
Muhamad Arif Muzaki <124240121+MUZAKI1453@users.noreply.github.com>
2024-12-31 21:18:03 +0700

    Add files via upload

---
+++ PythonProject/Elif_statement.py
@@ -0,0 +1,34 @@
+# elif = else if statement
+
+nama = input("masukan pemain abroad timnas indonesia :")
+
+if nama == "idzes":
+    print("venezia player")
+elif nama == "verdonk":
+    print("nec nijmegen player")
+elif nama == "haye":
+    print("almare player")
+elif nama == "hilgers":
+    print("twente player")
+elif nama == "diks":
+    print("copanhagen player")
+elif nama == "romeny":
+    print("utrecht player")
+elif nama == "marselino":
+    print("oxford player")
+elif nama == "pattynama":
+    print("eupen player")
+elif nama == "walsh":
+    print("machellen player")
+elif nama == "oratmangoen":
+    print("dender player")
+elif nama == "arhan":
+    print("suwon player")
+elif nama == "hubner":
+    print("wolverhampton player")
+elif nama == "asnawi":
+    print("port player")
+elif nama == "eliano":
+    print("pec zwole player")
+else :
+    print("no data on database")
