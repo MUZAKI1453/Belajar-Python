5ed6e875f3103fa143e777af5e6a68890dd20750
Muhamad Arif Muzaki <124240121+MUZAKI1453@users.noreply.github.com>
2025-01-01 21:48:35 +0700

    Add files via upload

---
+++ PythonProject/menampilkan_kalender.py
@@ -0,0 +1,10 @@
+# membuat calender dengan memanfaatkan library dari python
+import calendar
+import datetime as dt
+
+yy = 2025 #tahun
+mm = 1    #bulan
+
+# display calender
+print(calendar.month(yy,mm))
+print(dt.date.today())
